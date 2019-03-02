d4955e02c2b6a6442974c7ec870bde3769cdef40
Sherif Shendidy <sherif@shendidy.com>
2019-03-02 22:28:13 +0000

    :white_check_mark: test: signing up test failing to redirect.

--- spec/features/only_clicking_sign_up_spec.rb
+++ spec/features/only_clicking_sign_up_spec.rb
@@ -7,12 +7,13 @@ RSpec.feature "On signing up", type: :feature do
     fill_in "user[password]", with: ""
     fill_in "user[password_confirmation]", with: ""
     click_button ("Sign up")
-    expect(page).to have_content("2 errors prohibited this user from being saved:")
+    expect(page).to have_content("3 errors prohibited this user from being saved:")
   end
 
   scenario "successfully user is redirected to posts page" do
     visit "/users/sign_up"
-    fill_in "user[email]", with: "user1"
+    fill_in "user[user_name]", with: "user1"
+    fill_in "user[email]", with: "user1@email.com"
     fill_in "user[password]", with: "123456"
     fill_in "user[password_confirmation]", with: "123456"
     click_button ("Sign up")
