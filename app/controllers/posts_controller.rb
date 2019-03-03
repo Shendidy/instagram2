e3cc5f02651a3dff7de33fb0cd865cc9c4da7518
Sherif Shendidy <sherif@shendidy.com>
2019-03-03 02:37:52 +0000

    :white_check_mark: feat: Users can add posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,26 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  end
-
-  def show
+    @posts = Post.all.order("updated_at DESC")
+    @post = Post.new
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to :action => 'index'
+    else
+      flash.now[:notice] = "Error saving your new note, please try again!"
+      redirect_to posts_url
+    end
+  end
+
+  def show
   end
 
   def edit
@@ -20,5 +31,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:message, :user_id)
   end
 end
