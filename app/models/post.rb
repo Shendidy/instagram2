8ec2cdf9d7a689fae74de6ec68848b789f1221c3
Sherif Shendidy <sherif@shendidy.com>
2019-03-03 06:58:54 +0000

    :alembic: feat: Can't create a post without adding picture

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   # For paperclip to handle images
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_presence_of :image # Ensures post must have images.
 
   def when(date)
     since = Time.now.to_i - date.to_i
