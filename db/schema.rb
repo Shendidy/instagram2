e465e660120db07a056701c42a8d00df240834e7
Sherif Shendidy <sherif@shendidy.com>
2019-03-03 17:50:24 +0000

    :card_file_box: fix: fixed likes and dislikes in posts db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190303161041) do
+ActiveRecord::Schema.define(version: 20190303174132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,8 @@ ActiveRecord::Schema.define(version: 20190303161041) do
     t.string "image_content_type"
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
-    t.integer "likes", default: 0
+    t.text "likes"
+    t.text "dislikes"
   end
 
   create_table "users", force: :cascade do |t|
