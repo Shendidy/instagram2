1a412400a70553de4b5fc600c3be3dadec3b5ad4
Sherif Shendidy <sherif@shendidy.com>
2019-03-03 16:16:02 +0000

    :card_file_box: feat: added likes to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190303050818) do
+ActiveRecord::Schema.define(version: 20190303161041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20190303050818) do
     t.string "image_content_type"
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "likes", default: 0
   end
 
   create_table "users", force: :cascade do |t|
